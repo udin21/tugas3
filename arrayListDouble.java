a40f8c5e6250ffa6f6e1c4d54ebf37e5abc39d1d
udin21 <44660987+udin21@users.noreply.github.com>
2018-11-01 12:30:33 +0800

    Create arrayListDouble.java

---
+++ arrayListDouble.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayListDouble {
+
+	public static void main(String[] args) {
+		ArrayList<Double>isi = new ArrayList<Double>();
+		isi.add(4.4);
+		isi.add(2.2);
+		isi.add(2.1);
+		System.out.println("arraylistdouble : "+isi.toString());
+		System.out.println("size : "+isi.size());
+
+
+	}
+
+}
