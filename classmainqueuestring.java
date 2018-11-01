c4f0594d57e827723f9fbeea7f680d89afe84306
udin21 <44660987+udin21@users.noreply.github.com>
2018-11-01 12:48:54 +0800

    Create classmainqueuestring.java

---
+++ classmainqueuestring.java
@@ -0,0 +1,16 @@
+public class classmainqueuestring {
+
+	
+	public static void main(String[] args) {
+		classqueuestring q=new classqueuestring();
+		q.cetak();
+		q.insert("udin");
+		q.cetak();
+		q.insert("aswad");
+		q.cetak();
+		q.insert("sadly ramli");
+		q.cetak();
+
+	}
+
+}
