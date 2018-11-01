ba00055182d7233e7f98b32105b5132310d0f212
udin21 <44660987+udin21@users.noreply.github.com>
2018-11-01 12:33:59 +0800

    Create arraylistfloat.java

---
+++ arraylistfloat.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class arraylistfloat {
+
+	
+	public static void main(String[] args) {
+		ArrayList<Float>isi = new ArrayList<Float>();
+		isi.add(3.2f);
+		isi.add(2.1F);
+		isi.add(4.1F);
+		System.out.println("arraylistfloat : "+isi.toString());
+		System.out.println("size : "+isi.size());
+
+	}
+
+}
