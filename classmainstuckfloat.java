50190c9ca353ef628d5fec4c0f4b904408aca512
udin21 <44660987+udin21@users.noreply.github.com>
2018-11-01 12:54:44 +0800

    Create classmainstuckfloat.java

---
+++ classmainstuckfloat.java
@@ -0,0 +1,17 @@
+public class classmainstuckfloat {
+
+	
+	public static void main(String[] args) {
+		classstuckfloat sc=new classstuckfloat();
+			sc.push(3.1f);
+			sc.push(3.2f);
+			sc.push(3.3f);
+			sc.cetak();
+			float d0=sc.pop();
+			System.out.println(d0);
+			sc.cetak();
+		
+
+	}
+
+}
